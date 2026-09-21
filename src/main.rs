508b829965afaf570678708d9c45b84cfa8d6a31
shoooogi <guilhermecomputation@gmail.com>
2026-09-21 13:45:52 -0300

    chore: initialize repository

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use dioxus::prelude::*;
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/main.css");
+const HEADER_SVG: Asset = asset!("/assets/header.svg");
+const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        
+    }
+}
+
