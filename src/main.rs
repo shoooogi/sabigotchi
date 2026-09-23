1bab4b75d2927bf106381c43446067041ed38d59
shoooogi <guilhermecomputation@gmail.com>
2026-09-22 23:56:44 -0300

    feat: add main box and title

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use dioxus::prelude::*;
+use dioxus_google_fonts::google_fonts;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
-const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
     dioxus::launch(App);
@@ -12,7 +11,35 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-        
+        document::Title { "Sabigotchi" }
+        document::Link { rel: "icon", href: FAVICON }
+        document::Stylesheet { href: MAIN_CSS }
+        document::Link { rel: "preconnect", href: "https://fonts.googleapis.com" }
+        { google_fonts!([
+            ("Inter Tight", wght = [1, 400])
+        ]) }
+
+        MainTitle{}
+        MainBox{}
     }
 }
 
+#[component]
+fn MainBox() -> Element {
+    rsx! {
+        div { id: "main-box", class: "box",
+            div { id: "sabi-box", class: "box",
+                
+            }
+        }
+    }
+}
+
+#[component]
+fn MainTitle() -> Element {
+    rsx! {
+        h1 { id: "main-title", class: "text",
+            "錆びごっち"
+        }
+    }
+}
