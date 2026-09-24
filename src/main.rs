7cf143d2cce5e8e0c46c51afeaa602967e059969
shoooogi <guilhermecomputation@gmail.com>
2026-09-23 23:56:28 -0300

    feat: redesign the main box

--- src/main.rs
+++ src/main.rs
@@ -16,20 +16,29 @@ fn App() -> Element {
         document::Stylesheet { href: MAIN_CSS }
         document::Link { rel: "preconnect", href: "https://fonts.googleapis.com" }
         { google_fonts!([
-            ("Inter Tight", wght = [1, 400])
+            ("Inter Tight", wght = ["200..800"]),
+            ("Sawarabi Gothic", wght = [400, 700, 800]),
+            ("Pixelify Sans", wght = ["400..700"]),
         ]) }
 
-        MainTitle{}
-        MainBox{}
+        MainTitle {}
+        MainBox {}
+        Credits {}
     }
 }
 
 #[component]
 fn MainBox() -> Element {
     rsx! {
-        div { id: "main-box", class: "box",
-            div { id: "sabi-box", class: "box",
-                
+        div { id: "main-box",
+            div { id: "screen", }
+            div { id: "control",
+                div { class: "div_button", 
+                    div { id: "choose", class: "button" }
+                    ArcText { text: "Choose" }
+                }
+                div { id: "enter", class: "button", "Enter" }
+                div { id: "cancel", class: "button", "Cancel" }
             }
         }
     }
@@ -38,8 +47,49 @@ fn MainBox() -> Element {
 #[component]
 fn MainTitle() -> Element {
     rsx! {
-        h1 { id: "main-title", class: "text",
-            "錆びごっち"
+        div { id: "div_main-title",
+            img { id: "main-logo", src: asset!("/assets/rustlogo.png") },
+
+            h1 { id: "main-title", class: "text",
+                span { id: "main-title_color", "錆び" }, "ごっち"
+            }
+        }
+    }
+}
+
+#[component]
+fn Credits() -> Element {
+    rsx! {
+        p { id: "credits", class: "text",
+            "Made by Guilherme 💛"
+        }
+    }
+}
+
+#[component]
+fn ArcText(text: String) -> Element {
+    rsx! {
+        svg {
+            view_box: "0 0 500 100",
+            class: "w-full",
+
+            defs {
+                path {
+                    id: "curve",
+                    d: "M 50 20 Q 250 90 450 20",
+                    fill: "none",
+                }
+            }
+
+            text {
+                class: "button-text",
+                text_anchor: "middle",
+                textPath {
+                    href: "#curve",
+                    start_offset: "50%",
+                    {text}
+                }
+            }
         }
     }
 }
